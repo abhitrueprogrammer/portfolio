98d770001316b815735ad25da72860feb85971ce
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-02-11 03:29:48 +0530

    added links to github and linkedin

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
 
 export default function Home() {
-  const name = "abhinav pant"
+  const name = "abhinav pant";
   return (
     <div className="grid text-lg grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello world from {name}
+      <div>
+      <p>Hello world from <span className="text-xl text-green-600">{name}</span></p>
+        <p>
+          My github:{" "}
+          <Link className="text-blue-400 underline" href="https://github.com/abhitrueprogrammer/">
+            abhitrueprogrammer
+          </Link>
+        </p>
+        <p>
+          My linkedIn:{" "}
+          <Link className="text-blue-400 underline" href="https://www.linkedin.com/in/abhinav-pant/">
+            abhinav-pant
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
