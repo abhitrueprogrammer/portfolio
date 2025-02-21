643a49e80e8a1fc0559d2b6e0ff5226721fb9d7a
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-02-21 18:24:05 +0530

    feat: changed github/linkedin links to icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import PinnedRepos from "./components/PinnedRepos";
+import {Github, Linkedin} from "lucide-react";
 export default function Home() {
   const name = "abhinav pant";
 
@@ -7,28 +8,30 @@ export default function Home() {
   return (
     <div className=" text-lg  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
-        <p>
-          Hello world from{" "}
-          <span className="text-xl text-green-600">{name}</span>
-        </p>
-        <p>
-          My github:{" "}
-          <Link
-            className="text-blue-400 underline"
-            href="https://github.com/abhitrueprogrammer/"
-          >
-            abhitrueprogrammer
-          </Link>
-        </p>
-        <p>
-          My linkedIn:{" "}
-          <Link
-            className="text-blue-400 underline"
-            href="https://www.linkedin.com/in/abhinav-pant/"
-          >
-            abhinav-pant
-          </Link>
-        </p>
+        <div className="flex justify-between">
+          <p>
+            Hello world from{" "}
+            <span className="text-xl text-green-600">{name}</span>
+          </p>
+          <div className="flex gap-4 mr-6">
+            <p>
+              <Link
+                className="text-white underline"
+                href="https://github.com/abhitrueprogrammer/"
+              >
+                <Github/>
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="text-blue-400 underline"
+                href="https://www.linkedin.com/in/abhinav-pant/"
+              >
+                <Linkedin />
+              </Link>
+            </p>
+          </div>
+        </div>
         <div className="flex gap-4 flex-wrap">
           <PinnedRepos />
         </div>
