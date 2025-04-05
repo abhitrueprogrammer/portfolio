f6621ef09cd1179d2b331bb7213969176108b6b6
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-04-05 16:04:12 +0530

    feat: fix margin

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const name = "abhinav pant";
 
   return (
-    <div className=" text-lg  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className=" text-lg  items-center justify-items-center min-h-screen   p-10 pt-2 font-[family-name:var(--font-geist-sans)]">
       <div>
         <div className="flex justify-between">
           <p>
