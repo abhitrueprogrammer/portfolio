8d4835adeb0aca7fe9c7fe2d80a3d00ebbc8315b
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-04-08 14:57:35 +0530

    feat: refetch repos after 60 seconds on server

--- app/components/PinnedRepos.tsx
+++ app/components/PinnedRepos.tsx
@@ -27,6 +27,8 @@ export interface Repository {
     }[];
   };
 }
+// refetch every 60 seconds
+export const revalidate = 60;
 
 export default async function PinnedRepos({className}: {className?: string}) {
   const repos = await getPinnedRepos("abhitrueprogrammer");
